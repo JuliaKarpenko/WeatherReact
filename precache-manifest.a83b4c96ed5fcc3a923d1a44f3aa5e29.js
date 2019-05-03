5022cfd9ff6956a053903b85b627df16507f3641
Admin <jule4ka.jd@gmail.com>
2019-05-03 14:14:16 +0300

    Updates

---
+++ precache-manifest.a83b4c96ed5fcc3a923d1a44f3aa5e29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d90a2d9c680b7843b8eb20ae8add0917",
+    "url": "/WeatherReact/static/media/bg.d90a2d9c.jpg"
+  },
+  {
+    "revision": "2dbca4f9dcc384591026",
+    "url": "/WeatherReact/static/js/runtime~main.b7e1807a.js"
+  },
+  {
+    "revision": "5042e82cb9aaa6b22b5d",
+    "url": "/WeatherReact/static/js/main.3fe46e4f.chunk.js"
+  },
+  {
+    "revision": "026c6186e44f53a35d0f",
+    "url": "/WeatherReact/static/js/2.6c706776.chunk.js"
+  },
+  {
+    "revision": "5042e82cb9aaa6b22b5d",
+    "url": "/WeatherReact/static/css/main.c1b16afd.chunk.css"
+  },
+  {
+    "revision": "026c6186e44f53a35d0f",
+    "url": "/WeatherReact/static/css/2.8976d12f.chunk.css"
+  },
+  {
+    "revision": "e88230ec6fa1d917e4afb374d83851c1",
+    "url": "/WeatherReact/index.html"
+  }
+];
